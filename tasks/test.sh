4c5cc6221a01f043da72609229473c402f39f126
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-02-01 13:10:12 -0500

    Add the test tasks

---
+++ tasks/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+export GOPATH=$(pwd)/gopath
+export PATH=$PATH:$GOPATH/bin
+
+cd gopath/src/github.com/cloudfoundry-community/cf-uaa-guard-service
+
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
+ginkgo -r
