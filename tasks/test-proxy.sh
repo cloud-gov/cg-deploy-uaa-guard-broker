58bdddbc433c69ca59e07cf9a9dbdf6dce7be264
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-02-01 15:24:49 -0500

    Update the proxy path and glide init

--- tasks/test-proxy.sh
+++ tasks/test-proxy.sh
@@ -5,11 +5,12 @@ set -e
 export GOPATH=$(pwd)/gopath
 export PATH=$PATH:$GOPATH/bin
 
-cd gopath/src/github.com/cloudfoundry-community/cf-uaa-guard-service
+cd gopath/src/github.com/cloudfoundry-community/cf-uaa-guard-proxy
 
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
 
+curl https://glide.sh/get | sh
 glide install
 
 ginkgo -r
