5653e4d11a2eb880efe1225688b96c3bb3db921c
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-02-01 14:57:15 -0500

    Remove the unused config carry over from s3 config

--- tasks/build.sh
+++ tasks/build.sh
@@ -2,15 +2,3 @@
 
 set -e
 set -u
-
-cat << EOF > credentials.json
-{
-  "username": "${AUTH_USERNAME}",
-  "password": "${AUTH_PASSWORD}"
-}
-EOF
-
-cp -r broker-src/. broker-src-built
-
-jq -s '.[0] * .[1]' broker-config/config-template.json credentials.json > \
-  broker-src-built/config.json
