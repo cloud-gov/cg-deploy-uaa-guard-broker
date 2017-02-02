c11d11e0d18b40422030c440d23d462e0faea500
Leonard Bogdonoff <rememberlenny@gmail.com>
2017-02-02 13:49:54 -0500

    Spruce merge yaml magic

--- tasks/build.sh
+++ tasks/build.sh
@@ -2,11 +2,21 @@
 
 set -e
  
-echo $GUARD_COOKIE_SECRET
-echo $GUARD_DEFAULT_CALLBACK_URL
-echo $GUARD_UAA_URL
-echo $GUARD_CLIENT_KEY
-echo $GUARD_CLIENT_SECRET
-echo $GUARD_BROKER_USERNAME
-echo $GUARD_BROKER_PASSWORD
-echo $GUARD_ROUTE_SERVICE_URL
+cat <<-EOF >> manifest.secret.yml
+applications:
+  - name: uaa-guard-broker
+    env:
+      GUARD_BROKER_USERNAME: ${GUARD_BROKER_USERNAME}
+      GUARD_BROKER_PASSWORD: ${GUARD_BROKER_PASSWORD}
+      GUARD_ROUTE_SERVICE_URL: ${GUARD_ROUTE_SERVICE_URL}
+  - name: uaa-guard-proxy
+    env:
+      GUARD_COOKIE_SECRET: ${GUARD_COOKIE_SECRET}
+      GUARD_DEFAULT_CALLBACK_URL: ${GUARD_DEFAULT_CALLBACK_URL}
+      GUARD_UAA_URL: ${GUARD_UAA_URL}
+      GUARD_CLIENT_KEY: ${GUARD_CLIENT_KEY}
+      GUARD_CLIENT_SECRET: ${GUARD_CLIENT_SECRET}
+EOF
+
+spruce merge broker-src/manifest.yml.sample \
+             manifest.sample.yml
